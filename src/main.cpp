1935b3474278150554aa4447bf7e7719f7c6adcb
Tóth Bálint <toth.balint1225@gmail.com>
2021-01-04 14:27:53 +0100

    Manual translation with the pixel_size constant

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 class Game : public gg::GameEngine {
 private:
 	struct Pos {
-		int x,y;
+		float x,y;
 	};
 	enum Direction {
 		RIGHT,UP,LEFT,DOWN
@@ -22,14 +22,14 @@ public:
 	}
 public:
 	virtual bool onCreate() override {
-		snake = {{width/2,height/2}};
+		snake = {{1.f,1.f}};
 		snakeDirection = RIGHT;
-		snakeVelocity = 1.f;
-		deltaTime = 0.f;
+		snakeVelocity = 1.5f;
 		return true;
 	}
 
 	virtual bool onUpdate(float elapsed) override {
+		snake.front().x += snakeVelocity*elapsed;	
 		return true;
 	}
 
@@ -38,7 +38,34 @@ public:
 	}
 private:
 	virtual bool onDraw(const Cairo::RefPtr<Cairo::Context>& cr) override {
+		for (auto it : snake) {
+			fillRect(cr,(it.x)*pixelSize,(it.y)*pixelSize,pixelSize,pixelSize,0.f,1.f,0.f);
+		}
+		grid(cr);
 		return true;
+	}
+	void fillRect(const Cairo::RefPtr<Cairo::Context>& cr,float x1,float y1,float w,float h,float r,float g,float b) {
+		cr->save();
+		cr->set_source_rgb(r,g,b);
+		cr->rectangle(x1,y1,w,h);
+		cr->fill_preserve();
+		cr->stroke();
+		cr->restore();
+	}
+	void grid(const Cairo::RefPtr<Cairo::Context>& cr) {
+		cr->save();
+		cr->set_source_rgb(1.f,1.f,1.f);
+		for (int i=0;i<=width;i++) {
+			cr->move_to(i*pixelSize,0);
+			cr->line_to(i*pixelSize,height*pixelSize);
+		}
+		for (int i=0;i<=height;i++) {
+			cr->move_to(0,i*pixelSize);
+			cr->line_to(width*pixelSize,i*pixelSize);
+		}
+
+		cr->stroke();
+		cr->restore();
 	}
 };
 
